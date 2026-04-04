ee6545629343ed23c164733f20a6db94e74b44de
Gaurav Kumar Jha <gauravkumarjha@users.noreply.github.com>
2026-04-04 21:48:23 +0530

    Simplify checkout extension to a basic contact banner

--- extensions/checkout-enhancements/src/Checkout.tsx
+++ extensions/checkout-enhancements/src/Checkout.tsx
@@ -1,47 +1,14 @@
-import type {} from "@shopify/ui-extensions/checkout/preact";
-import {
-  createElement,
-  render,
-} from "preact";
+import {Banner, reactExtension} from "@shopify/ui-extensions-react/checkout";
 
-import {
-  useApplyAttributeChange,
-  useAttributeValues,
-  useInstructions,
-} from "@shopify/ui-extensions/checkout/preact";
-
-export default function extension() {
-  render(<Extension />, document.body);
-}
+export default reactExtension(
+  "purchase.checkout.contact.render-after",
+  () => <Extension />,
+);
 
 function Extension() {
-  const [freeGiftRequested] = useAttributeValues(["requestedFreeGift"]);
-  const instructions = useInstructions();
-  const applyAttributeChange = useApplyAttributeChange();
-
-  async function onCheckboxChange(event: Event) {
-    const input = event.currentTarget as HTMLInputElement;
-    const isChecked = input.checked;
-
-    if (!instructions.attributes.canUpdateAttributes) {
-      console.error("Attributes cannot be updated in this checkout");
-      return;
-    }
-
-    const result = await applyAttributeChange({
-      key: "requestedFreeGift",
-      type: "updateAttribute",
-      value: isChecked ? "yes" : "no",
-    });
-
-    console.log("applyAttributeChange result", result);
-  }
-
   return (
-    <s-checkbox
-      checked={freeGiftRequested === "yes"}
-      onChange={onCheckboxChange}
-      label="I would like to receive a free gift with my order"
-    />
+    <Banner title="Hi, I'm calling">
+      Your app is activated.
+    </Banner>
   );
 }
